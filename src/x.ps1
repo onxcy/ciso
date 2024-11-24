cd5e7190cb83e825680160366ffb2622c293069e
onxcy <174684063+onxcy@users.noreply.github.com>
2024-11-24 09:14:07 +0100

    Add files via upload

--- src/x.ps1
+++ src/x.ps1
@@ -1,5 +1,6 @@
 $disk = (Get-Disk | Where-Object -Property BusType -EQ 'USB' | Select-Object -Index 0)
 if ($null -eq $disk) { exit 1 }
 $disk | Clear-Disk -RemoveData -RemoveOEM -confirm:$false -PassThru | Initialize-Disk -PartitionStyle GPT | Out-Null
-$disk | New-Partition -Size 7GB -AssignDriveLetter | Format-Volume -FileSystem NTFS | Out-Null
+$size = $disk.Size * 0.95
+$disk | New-Partition -Size $size -AssignDriveLetter | Format-Volume -FileSystem NTFS | Out-Null
 ($disk | New-Partition -Size 1MB).AccessPaths
